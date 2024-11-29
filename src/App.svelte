e2f847183b1cd2e23a9209669975ad0024e5879b
Alejandro Sanchez Bastida <alejandro.sanchez.bastida@gmail.com>
2024-11-29 08:47:22 +0100

    Initial

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import GeneratedValue from "./lib/GeneratedValue.svelte";
+  import { dniGenerator } from "./lib/generators";
+
+  let dniGenerated = $state("Generar DNI");
+</script>
+
+<main>
+  <header>
+    <h1 class="text-9xl text-center">Generadores</h1>
+  </header>
+
+  <div class="card">
+    <GeneratedValue
+      content={dniGenerated}
+      on:click={() => (dniGenerated = dniGenerator())}
+    />
+  </div>
+</main>
---
+++ src/lib/generators.ts
@@ -0,0 +1,12 @@
+export const dniGenerator = (): string => {
+  const letterMap = "TRWAGMYFPDXBNJZSQVHLCKE".split("");
+
+  const n = generateNumber(1e7, 99_999_999);
+
+  const letter = letterMap[n % 23];
+
+  return n.toString() + letter;
+};
+
+const generateNumber = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min) + min);
